30063b95400f5b49ecf6da7b13b69ea271481a5f
Rajaniraiyn <rajaniraiyn@gmail.com>
2022-08-07 09:55:05 +0530

    refactor(main.ts): add hydration in svelte and Improve CSS imports
    Added svelte hydration option and Improved CSS stylesheets imports using import assert

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import "./app.css";
-import "./styles/index.css";
+import "./app.css" assert { type: "css" };
+import "./styles/index.css" assert { type: "css" };
 import App from "./App.svelte";
 
 const app = new App({
   target: document.getElementById("app"),
+  hydrate: true,
 });
 
 export default app;
