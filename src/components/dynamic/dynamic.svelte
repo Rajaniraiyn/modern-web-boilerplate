6f6ebc6bf3c3acd19271d35b1c44dbe2daf6baec
Rajaniraiyn <rajaniraiyn@gmail.com>
2022-08-07 22:58:52 +0530

    feat(dynamic.svelte): add CSS loader to loading state in dynamic components
    Added CSS dots loader for loading state in dynamic elements to increase interactivity

--- src/components/dynamic/dynamic.svelte
+++ src/components/dynamic/dynamic.svelte
@@ -7,9 +7,49 @@
 </script>
 
 {#await import(`../../${(view ? "views" : "components") + "/" + name}.svelte`)}
-  Loading....
+  <div class="balls">
+    <div />
+    <div />
+    <div />
+  </div>
 {:then { default: component }}
   <svelte:component this={component} {...parmameters}>
     <slot />
   </svelte:component>
 {/await}
+
+<style>
+  .balls {
+    width: 3.5em;
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .balls div {
+    width: 0.8em;
+    height: 0.8em;
+    border-radius: 50%;
+    background-color: var(--accent, black);
+    transform: translateY(-100%);
+    animation: wave 0.8s ease-in-out alternate infinite;
+  }
+
+  .balls div:nth-of-type(1) {
+    animation-delay: -0.4s;
+  }
+
+  .balls div:nth-of-type(2) {
+    animation-delay: -0.2s;
+  }
+
+  @keyframes wave {
+    from {
+      transform: translateY(-100%);
+    }
+    to {
+      transform: translateY(100%);
+    }
+  }
+</style>
