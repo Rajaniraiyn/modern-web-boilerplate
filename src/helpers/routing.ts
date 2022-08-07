b276710f19d855b481df33a0b85c92af54479b81
Rajaniraiyn <rajaniraiyn@gmail.com>
2022-08-07 16:29:46 +0530

    fix(routing): fix routing in embeded pages
    Fixed the issue with embeded pages routing hash mode

--- src/helpers/routing.ts
+++ src/helpers/routing.ts
@@ -1,3 +1,5 @@
 import { router } from "tinro";
 
-router.mode.hash();
+const { mode, goto, location } = router;
+
+export { mode, goto, location };
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import "./app.css" assert { type: "css" };
 import "./styles/index.css" assert { type: "css" };
 import App from "./App.svelte";
 
+import { mode } from "./helpers";
+
+mode.hash();
+
 const app = new App({
   target: document.getElementById("app"),
   hydrate: true,
