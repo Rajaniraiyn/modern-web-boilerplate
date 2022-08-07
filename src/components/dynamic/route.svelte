59a7f299d049bf7e2fd782600d4fdad3e4662f88
Rajaniraiyn <rajaniraiyn@gmail.com>
2022-08-07 14:54:58 +0530

    feat: add 404 fallback
    Added 404 fallback page with animations

--- src/components/dynamic/route.svelte
+++ src/components/dynamic/route.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { Route } from "tinro";
   import Dynamic from "./dynamic.svelte";
+  import E404 from "./404.svelte";
 
   export let path: string;
   export let view: string = null;
@@ -10,4 +11,6 @@
   <Dynamic name={view} view={true} />
 </Route>
 
-<Route fallback>Page Doesn't Exists</Route>
+<Route fallback>
+  <E404 />
+</Route>
